e7bce6a8f2c010fe2d5ff2a02bc270afc95a0741
stoiss <jb@stoiss.net>
2023-07-29 10:00:00 +0300

    Change verstion 0.8.21 -> 0.8.19

--- hardhat.config.js
+++ hardhat.config.js
@@ -33,7 +33,7 @@ module.exports = {
         }
     },
     solidity: {
-        version: "0.8.21",
+        version: "0.8.19",
         settings: {
             optimizer: {
                 enabled: true
